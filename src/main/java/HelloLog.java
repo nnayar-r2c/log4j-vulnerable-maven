3f39cb75caddf0e53d835ef2b54c1b21dc36c9fc
dash4ast <support@dash4ast.com>
2023-02-24 11:50:30 +0100

    add new vulns

--- src/main/java/HelloLog.java
+++ src/main/java/HelloLog.java
@@ -14,10 +14,16 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import java.net.http.HttpRequest;
 import java.net.PasswordAuthentication;
 
+import org.datanucleus.api.jdo.JDOPersistenceManagerFactory;
+import javax.jdo.PersistenceManagerFactory;
+
 //Test class
 public class HelloLog {
 
     private static final Logger logger = LogManager.getLogger();
+
+    private PersistenceManagerFactory pmf;
+    private String pw = "asdf";
 
     public static void main(String[] args) {
       String userInput = "${jndi:http://localhost/AAAA/BBBB}";
@@ -70,5 +76,29 @@ public class HelloLog {
                 }
             })
             .build();
+    }
+
+    /**
+     * @throws SQLException
+     */
+    @Before
+    public void setUp() throws SQLException {
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put("datanucleus.autoCreateSchema", true);
+        props.put("datanucleus.rdbms.statementBatchLimit", 0);
+
+        pmf = new JDOPersistenceManagerFactory(props);
+        pmf.setConnectionDriverName(jdbcDriver.class.getName());
+        pmf.setConnectionURL("jdbc:hsqldb:mem:testdb;hsqldb.sqllog=3");
+        pmf.setConnectionUserName("SA");
+
+        // ruleid: hardcoded-connection-password
+        pmf.setConnectionPassword("asdf");
+
+        // ruleid: hardcoded-connection-password
+        pmf.setConnectionPassword(pw);
+
+        // ok: hardcoded-connection-password
+        pmf.setConnectionPassword("");
     }
 }
